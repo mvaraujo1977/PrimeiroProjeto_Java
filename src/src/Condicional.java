d456d8ca64bba6cffd496c6b067a66365470474e
mvaraujo1977 <marcelovianadearaujo@gmail.com>
2023-12-02 23:52:41 -0300

    criado o arquivo main

---
+++ src/src/Condicional.java
@@ -0,0 +1,23 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = false;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "normal";
+
+
+        if (anoDeLancamento >= 2022){
+            System.out.println("Lançamento que os clientes estao curtindo!");
+        }
+        else {
+            System.out.println("Não é lançamento!");
+        }
+        // esse é o ou || e esse é o and &&
+        if (incluidoNoPlano || tipoPlano.equals("plus")){
+            System.out.println("Filme liberado!");
+
+        }else{
+            System.out.println("Deve pagar locação!");
+        }
+    }
+}
