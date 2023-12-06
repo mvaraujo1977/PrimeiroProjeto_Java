c031934741bb65ad627390e07fb6d7c249bba68a
Marcelo Viana <marcelovianaaraujo@hotmail.com>
2023-12-05 22:46:08 -0300

    criado arquivo leitura

--- src/src/Leitura.java
+++ src/src/Leitura.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Leitura {
     public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito");
+        String filme = leitura.nextLine();
+
+        System.out.println("Digite Ano");
+        int anoDeLancamento = leitura.nextInt();
+
+        System.out.println("Digite Avaliacao");
+        double avaliacao = leitura.nextDouble();
+
+        System.out.println("O filme digitado foi: "+ filme);
+        System.out.println("O ano digitado foi: "+ anoDeLancamento);
+        System.out.println("A avaliacao digitado foi: "+ avaliacao);
+
+
+
+
+
 
     }
 
